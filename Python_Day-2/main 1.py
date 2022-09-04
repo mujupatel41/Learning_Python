d468cba58ad151270d486c458956979094edbcd5
mujupatel <mujupatel41@gmail.com>
2022-09-04 12:48:45 +0530

    Python Day 2

---
+++ Python_Day-2/main 1.py
@@ -0,0 +1,13 @@
+n1,n2 = map(int,input().split(" "))
+if n1>n2:
+    print("true")
+else:
+    print("false")
+if n1<n2:
+    print("true")
+else:
+    print("false")
+if n1==n2:
+    print("true")
+else:
+    print("false")
